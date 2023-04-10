2501db07064444ce7b856854a98c97c0cf980f60
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2023-04-10 19:31:16 +0000

    deployment commit

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"]="postgres://rlmynxsm:B_HMezkjdaTarD5y1AmU7nBmT9DoVxUe@trumpet.db.elephantsql.com/rlmynxsm"
 os.environ["SECRET_KEY"]="django-insecure-0eg3is7s+dg)xv)q!!ofhth34eyl^xshv*&hd2x9$z0$k(bd6a"
+os.environ["CLOUDINARY_URL"]="cloudinary://948949725979254:t24F3U0RxeG0sSSR5SeYeBN1k3U@dijdx1fv1"
