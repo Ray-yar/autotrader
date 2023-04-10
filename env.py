8d69c51582838d5192fd8d58fcb0120cbed48d50
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2023-04-10 19:12:03 +0000

    add env file

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"]="postgres://rlmynxsm:B_HMezkjdaTarD5y1AmU7nBmT9DoVxUe@trumpet.db.elephantsql.com/rlmynxsm"
+os.environ["SECRET_KEY"]="django-insecure-0eg3is7s+dg)xv)q!!ofhth34eyl^xshv*&hd2x9$z0$k(bd6a"
