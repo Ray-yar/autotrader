40d5133a535857f3b67bda888ccce8bab11be8c7
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2023-04-11 13:02:24 +0000

    add new features in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import VehicleModel
+from .models import Vehicle
+from django_summernote.admin import SummernoteModelAdmin
 
 
-# Register your models here.
-admin.site.register(VehicleModel)
+@admin.register(Vehicle)
+class VehicleAdmin(SummernoteModelAdmin):
+    
+    list_filter = ('status', 'make', 'created_at')
+    search_fields = ['make', 'model', 'year', 'body', 'fuel']
+    list_display = ('make', 'model', 'year', 'body', 'fuel', 'status', 'approved', 'created_at')
+    summernote_fields = ('content')
+    actions = ['approve_vehicle']
+
+    def approve_vehicle(self, request, queryset):
+        queryset.update(approved=True)
+        queryset.update(status=1)
--- shop/models.py
+++ shop/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+STATUS = ((0, 'Draft'), (1, 'Published'))
 # Create your models here.
-class VehicleModel(models.Model):
+class Vehicle(models.Model):
     make = models.CharField(max_length=128, blank=False)
     model = models.CharField(max_length=128, blank=False)
     color = models.CharField(max_length=128, blank=False)
@@ -15,6 +16,9 @@ class VehicleModel(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     featured_photo = CloudinaryField('image', default='placeholder')
     transmission = models.CharField(max_length=128)
+    content = models.TextField(default="No Details")
+    status = models.IntegerField(choices=STATUS, default=0)
+    approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
