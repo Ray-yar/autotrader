025374bd7b13a603d8d5cc7d701c59a16f2623be
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2023-04-15 13:51:02 +0000

    add details page & All Auth Packages

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.VehicleView.as_view(), name="home")
+    path('', views.VehicleView.as_view(), name="home"),
+    path('<str:id>/', views.VehicleDetail.as_view(), name='vehicle_detail')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Vehicle
 
 # Create your views here.
@@ -7,5 +7,19 @@ class VehicleView(generic.ListView):
     model = Vehicle
     object = Vehicle.objects.filter(status=1).order_by('created_at')
     template_name = "index.html"
-    paginate_by = 6
+    paginate_by = 8
 
+
+class VehicleDetail(View):
+
+    def get(self, request, id, *args, **keyargs):
+        queryset = Vehicle.objects.filter(status=1)
+        vehicle = get_object_or_404(queryset, id=id)
+
+        return render(
+            request, 
+            'vehicle_details.html', 
+            {
+                'vehicle' : vehicle,
+            },
+        )
