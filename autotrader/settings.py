06f404b90fe915af72f3f0c4d21390e79cecbe7c
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2023-07-21 20:37:25 +0000

    Add new changes on the details page's UI, include crud implementation for reviewer

--- autotrader/settings.py
+++ autotrader/settings.py
@@ -29,11 +29,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['autotrader.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['autotrader.herokuapp.com', 'localhost', '127.0.0.1', "8000-rayyar-autotrader-sfkqxmk8yrz.ws-us102.gitpod.io"]
 
 
 # Application definition
